e83c158d0a4affe33a09b947383759c6d6e2cd21
czndy <guicozendey@gmail.com>
2023-09-19 01:11:49 -0300

    add gitignore

--- temp_hum_nodejs/temp_electron.ino
+++ temp_hum_nodejs/temp_electron.ino
@@ -6,26 +6,25 @@ DHT dht(DHT_PIN,DHT_TYPE);
 
 //the code here will be executed only once. Here we are initializing the Serial and the DHT sensor.
 void setup() {
-  Serial.begin(9600);
-  dht.begin();
+    Serial.begin(9600);
+    dht.begin();
 }
 
 //the code here will be executed in a loop. The delay function sets the time interval that this loop will be executed.
 void loop() {
   
-  //here we are organizing the reading of the sensor data into two separate variables.
-  float temp = dht.readTemperature();
-  float hum = dht.readHumidity();
+    //here we are organizing the reading of the sensor data into two separate variables.
+    float temp = dht.readTemperature();
+    float hum = dht.readHumidity();
 
-  Serial.print("Temp: ");
-  Serial.print(temp, 1);
-  // Serial.print((char)223); // "°" symbol
-  Serial.println(" C");
-  Serial.print("Humidity: ");
-  Serial.print(hum, 1);
-  Serial.println("%");
-  Serial.println("###############");
+    Serial.print("Temp: ");
+    Serial.print(temp, 1);
+    // Serial.print((char)223); // "°" symbol
+    Serial.println(" C");
+    Serial.print("Humidity: ");
+    Serial.print(hum, 1);
+    Serial.println("%");
+    Serial.println("###############");
 
-  delay(1000);
-  
+    delay(1000);
 }
